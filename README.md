3c764c84089b529bf7664beaabbfe24ec6720972
anAwsomePerson <an.awsome.person.99@gmail.com>
2021-05-06 11:20:46 -0700

    wrote readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Black-White-png
- looks at decent color pictures and draws ugly black-white pictures
+looks at decent color pictures and draws ugly black-white pictures
+
+How to install:
+
+Make sure your computer has a command line app. Mac and Linux systems should already have one, and Windows users should install WSL using these instructions: https://docs.microsoft.com/en-us/windows/wsl/install-win10
+
+Install Python 3 on your computer and make sure that your command line app recognizes the "python3" and "pip" commands.
+
+Enter "pip install png" in your command line app.
+
+Download the black-white-png.py file from this Github project.
+
+
+
+How to use:
+
+Put a .png file and a copy of black-white-png.py in the same folder.
+
+Open your command line app and use the "cd" command so that folder becomes your working directory.
+
+Enter "python3 black-white-png.py <brightness> <in file name> <out file name>".
+
+Brightness can be any integer between 0 and 256 (exclusive)\*. Every pixel becomes black if its brightness is less than the brightness you entered.
+
+Example: The command "python3 black-white-png.py 128 gate.png gate128.png" creates a file named gate128.png. In that file, every pixel is black if its brightness was less than 128 in gate.png.
+
+\*Technically it will work with any integer, but integers 0 or less will simply result in a white rectangle, and integers 256 or greater will simply result in a black rectangle.
--- black-white-png.py
+++ black-white-png.py
@@ -1,29 +1,46 @@
+#please read the README.md file before running this code
 import png
 import sys
-inTuple = png.Reader(sys.argv[2]).read()
-outImage = []
 
-for a in inTuple[2]:
+def convertRow(pixelSize, colorSize, inRow):
     outRow = []
     brightness = 0
-    #print(type(b))
-    for b in range(0, len(a)):
-        if(b % 4 == 2):
-            brightness += float(a[b])
 
-            if(int(brightness) < int(sys.argv[1]) * 3):
+    for b in range(0, len(inRow)):
+        if(b % pixelSize == colorSize - 1):
+            brightness += float(inRow[b])
+
+            if(int(brightness) < int(sys.argv[1]) * colorSize):
                 outRow = outRow + [0]
             else:
                 outRow = outRow + [255]
             
             brightness = 0
-        elif(b % 4 == 3):
-            outRow += [a[b]]
-        else:
-            brightness += float(a[b])
+        elif(b % pixelSize < colorSize - 1):
+            brightness += float(inRow[b])
+    
+    return([outRow])
+
+inTuple = png.Reader(sys.argv[2]).read()
+inImage = (list(inTuple[2])).copy()
+outImage = []
+
+for a in inImage:
+    if('palette' in inTuple[3].keys()):
+        inRow = ()
+
+        for b in a:
+            inRow = inRow + inTuple[3]['palette'][int(b)]
         
-    outImage = outImage + [outRow]
+        outImage = outImage + convertRow(3, 3, inRow)
+    elif(inTuple[3]['planes'] == 3):
+        outImage = outImage + convertRow(3, 3, a)
+    elif(inTuple[3]['planes'] == 4):
+        outImage = outImage + convertRow(4, 3, a)
+    else:
+        print("""I was not programmed to convert this picture. 
+        Please send this picture to my programmer so he/she/they/it can update me.""")
 
 outFile = open(sys.argv[3], 'wb')
-png.Writer(inTuple[0], inTuple[1], greyscale = True, alpha = True).write(outFile, outImage)
+png.Writer(inTuple[0], inTuple[1], greyscale = True, alpha = False).write(outFile, outImage)
 outFile.close()
